3b75eb2c4dec7fc88bbf330edc600bc4ebdd0854
ReiLaciste <146046177+ReiLaciste@users.noreply.github.com>
2023-12-01 20:35:08 -0800

    reformatted layout, renamed variables, added styling, added dark mode features

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const nukebutton = document.querySelector('.nukebutton');
-
 // Grabs text
 const textinput = document.querySelector('.textarea');
 // Grabs button input
-const buttoninput = document.querySelector('.buttoninput');
+const buttoninput = document.querySelector('.add-new-item-btn');
 // Grabs the list of items in todo list
-const itemlist = document.querySelector('.todolist');
+const itemlist = document.querySelector('#items-container');
 
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 
@@ -16,49 +15,57 @@ function clickButton(event) {
 } 
 
 function addItem() {
-    // creates an element for the new item
-    const newItem = document.createElement('div');
-
-    // Generate a unique ID for the new item
-    const itemId = 'item' + Date.now();
-
-    // adds a class 'newItem' to the element
-    newItem.classList.add('newItem');
-    // Set the unique ID to the new item
-    newItem.id = itemId;
-
-    newItem.draggable = true; //makes items draggable
-
-    // Add a black border to the newItem
-    
-    
-    // creates an element for the text in our item
-    const itemContent = document.createElement('p');
-    // adds a class 'item' to the element
-    itemContent.classList.add('itemContent');
-    // the text of the element 'p' is what the text is in our query selector
-    itemContent.innerText = textinput.value;
-    // add the itemContent to the newItem div
-    newItem.appendChild(itemContent);
-
-    // if textbox is empty, do nothing
+     // if textbox is empty, do nothing
     if (textinput.value === '') {
-        // alert("Please input a task!");
+        gsap.to("#list-container", .1, {x: -4})
+        gsap.to("#list-container", .1, {delay: .1, x: 4})
+        gsap.to("#list-container", .1, {delay: .2, x: -4})
+        gsap.to("#list-container", .1, {delay: .3, x: 4})
+        gsap.to("#list-container", .1, {delay: .4, x: 0})
         return;
     } else {
+        // creates an element for the new item
+        const newItem = document.createElement('div');
+
+        // Generate a unique ID for the new item
+        const itemId = 'item' + Date.now();
+
+        // adds a class 'newItem' to the element
+        newItem.classList.add('newItem');
+        // Set the unique ID to the new item
+        newItem.id = itemId;
+
+        newItem.draggable = true; //makes items draggable    
         // create a button to check off items
         const checkbutton = document.createElement("button");
         // makes the text inside button to say check
-        checkbutton.innerHTML = '<i class="fa-solid fa-check"></i>';
+        checkbutton.innerHTML = '<i class="fa-regular fa-circle-check fa-lg"></i>';
         // adds class
         checkbutton.classList.add("check-button");
         // adds checkButton to the newItem
         newItem.appendChild(checkbutton);
+        
+        // creates an element for the text in our item
+        const itemContent = document.createElement('p');
+        // adds a class 'item' to the element
+        itemContent.classList.add('itemContent');
+        // the text of the element 'p' is what the text is in our query selector
+        itemContent.innerText = textinput.value;
+        // add the itemContent to the newItem div
+        newItem.appendChild(itemContent);
+
+        // // Create a date input field
+        // const dateInput = document.createElement('input');
+        // dateInput.setAttribute('type', 'date');
+        // // Add a class for styling
+        // dateInput.classList.add('date-input'); 
+        // // Add the dateInput to the newItem
+        // newItem.appendChild(dateInput);      
 
         // creates a button to delete items
         const trashbutton = document.createElement("button");
         // makes the text inside to say trash
-        trashbutton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+        trashbutton.innerHTML = '<i class="fa-solid fa-trash btn"></i>';
         // adds class
         trashbutton.classList.add("trash-button");
         // add trashbutton to the newItem
@@ -68,10 +75,13 @@ function addItem() {
         itemlist.appendChild(newItem);
         // reset the text in textbox to blank to prep next input
         textinput.value = '';
+
+        gsap.to("#list-container, .newItem", .3, {paddingBottom: 30, ease: Back.easeOut})
+        gsap.to("#list-container, .newItem", .3, {delay: .15, paddingBottom: 8, y: 0, ease: Back.easeOut})
+
         saveData();
     }
 }
-
 
 function deleteItem(event) {
     // the item focused is the item that was clicked on
@@ -81,6 +91,10 @@ function deleteItem(event) {
         const todolistitem = item.parentElement
         // remove item
         todolistitem.remove()
+
+        gsap.to("#list-container, .newItem", 0, {paddingBottom: 26})
+        gsap.to("#list-container, .newItem", .3, {paddingBottom: 4, ease: Back.easeOut})
+
         saveData();
     }
 }
@@ -114,7 +128,6 @@ function handleDragStart(event) {
     //store the dragged item
     event.dataTransfer.setData('text/plain', event.target.id);
     event.target.classList.add('dragging');
-
 }
 
 function handleDragOver(event) {
@@ -150,6 +163,10 @@ function handleDragEnd(event) {
 
 function darkToggle(event) {
     document.body.classList.toggle('dark-mode');
+    document.getElementById('list-container').classList.toggle('dark-mode');
+    document.getElementById('title').classList.toggle('dark-mode');
+    document.querySelector('.newItem').classList.toggle('dark-mode');
+    document.querySelector('.trash-button').classList.toggle('dark-mode');
     saveData();
 }
 
@@ -176,5 +193,10 @@ itemlist.addEventListener('dragover', handleDragOver);
 itemlist.addEventListener('drop', handleDrop);
 itemlist.addEventListener('dragend', handleDragEnd);
 
+// dark mode event listener
 darkModeToggle.addEventListener('click', darkToggle);
 showTask();
+
+checkbutton.addEventListener('click', () => {
+    checkbutton.toggleClass('#27FDC7', black);
+})
