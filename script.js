f1fe4bd0ca85a273c054b2110369dac94a2e083b
ReiLaciste <146046177+ReiLaciste@users.noreply.github.com>
2023-12-05 00:37:19 -0800

    fixed drag and drop

--- script.js
+++ script.js
@@ -8,12 +8,6 @@ class TodoList {
             this.buttonInput = document.querySelector('.add-new-item-btn');
             this.itemList = document.querySelector('#items-container');
             this.darkModeToggle = document.getElementById('dark-mode-toggle');
-
-            this.handleDragStart = this.handleDragStart.bind(this);
-            this.handleDragOver = this.handleDragOver.bind(this);
-            this.handleDragLeave = this.handleDragLeave.bind(this);
-            this.handleDrop = this.handleDrop.bind(this);
-            this.handleDragEnd = this.handleDragEnd.bind(this);
         
             this.initiateEventListeners();
             this.showTask();
@@ -25,21 +19,17 @@ class TodoList {
     }
 
     initiateEventListeners() { 
-        this.buttonInput.addEventListener('click', (event) => this.clickButton(event));
+        this.buttonInput.addEventListener('click', (event) => this.addItem(event));
         this.itemList.addEventListener('click', (event) => this.deleteItem(event));
         this.itemList.addEventListener('click', (event) => this.checkItem(event));
         this.textInput.addEventListener('keypress', (event) => this.enterKey(event));
         this.dateInput.addEventListener('keypress', (event) => this.enterKey(event));
         this.nukeButton.addEventListener('click', () => this.moveDiv());
-        this.itemList.addEventListener('dragover', (event) => this.handleDragOver(event));
-        this.itemList.addEventListener('drop', (event) => this.handleDrop(event));
-        this.itemList.addEventListener('dragend', (event) => this.handleDragEnd(event));
-        this.darkModeToggle.addEventListener('click', () => this.darkToggle());
-    }
 
-    clickButton(event) {
-        event.preventDefault();
-        this.addItem()
+        this.darkModeToggle.addEventListener('click', () => this.darkToggle());
+
+        this.itemList.addEventListener('dragstart', (event) => this.handleDragStart(event));
+        this.itemList.addEventListener('drop', (event) => this.handleDrop(event));
     }
 
     showTask() {
@@ -55,7 +45,7 @@ class TodoList {
     }
 
     addItem() {
-            // if textbox is empty, do nothing
+        // if textbox is empty, do nothing
         if (this.textInput.value === '') {
             gsap.to("#list-container", .1, {x: -4})
             gsap.to("#list-container", .1, {delay: .1, x: 4})
@@ -66,16 +56,17 @@ class TodoList {
         } else {
             // creates an element for the new item
             const newItem = document.createElement('div');
-
+    
             // Generate a unique ID for the new item
             const itemId = 'item' + Date.now();
-
+    
             // adds a class 'newItem' to the element
             newItem.classList.add('newItem');
             // Set the unique ID to the new item
             newItem.id = itemId;
-
-            newItem.draggable = true; //makes items draggable    
+    
+            newItem.draggable = true; // makes items draggable    
+            
             // create a button to check off items
             const checkbutton = document.createElement("button");
             // makes the text inside button to say check
@@ -84,17 +75,20 @@ class TodoList {
             checkbutton.classList.add("check-button");
             // adds checkButton to the newItem
             newItem.appendChild(checkbutton);
-
+    
+            // creates a container for itemContent and itemDate
+            const itemContainer = document.createElement('div');
+            itemContainer.classList.add('item-container');
+    
             // creates an element for the text in our item
             const itemContent = document.createElement('p');
             // adds a class 'item' to the element
             itemContent.classList.add('itemContent');
             // the text of the element 'p' is what the text is in our query selector
             itemContent.innerText = this.textInput.value;
-            // add the itemContent to the newItem div
-            newItem.appendChild(itemContent);
-
-
+            // add the itemContent to the itemContainer div
+            itemContainer.appendChild(itemContent);
+    
             // creates an element for the text in our item
             const userDate = document.getElementById('date-picker').value;
             const itemDate = document.createElement('p');
@@ -108,34 +102,37 @@ class TodoList {
                     month: 'long',
                     day: '2-digit'
                 });
-                // add the itemContent to the newItem div
-                newItem.appendChild(itemDate);
+                // add the itemContent to the itemContainer div
+                itemContainer.appendChild(itemDate);
             }
-
+    
+            // add the itemContainer to the newItem div
+            newItem.appendChild(itemContainer);
+    
             // creates a button to delete items
             const trashbutton = document.createElement("button");
             // makes the text inside to say trash
             trashbutton.innerHTML = '<i class="fa-solid fa-trash btn"></i>';
             // adds class
             trashbutton.classList.add("trash-button");
-
+    
             trashbutton.classList.toggle('dark-mode', document.body.classList.contains('dark-mode'));
-
+    
             // add trashbutton to the newItem
             newItem.appendChild(trashbutton);
-
+    
             // add the new item to the list of items
             this.itemList.appendChild(newItem);
-
+    
             newItem.classList.toggle('dark-mode', document.body.classList.contains('dark-mode'));
-
+    
             // reset the text in textbox to blank to prep next input
             this.textInput.value = '';
             this.dateInput.value = '';
-
+    
             gsap.to("#list-container, .newItem", .3, {paddingBottom: 30, ease: Back.easeOut})
             gsap.to("#list-container, .newItem", .3, {delay: .15, paddingBottom: 8, y: 0, ease: Back.easeOut})
-
+    
             this.saveData();
         }
     }
@@ -196,54 +193,43 @@ class TodoList {
         }
     }
 
-    // handleDragStart(event) {
-    //     console.log('Drag start:', event.target);
+    handleDragStart(event) {
+        console.log('Drag start:', event.target);
 
-    //     event.dataTransfer.setData('text/plain', event.target.id);
-    // }
+        event.dataTransfer.setData('text/plain', event.target.id);
+    }
 
-    // handleDragOver(event) {
-    //     console.log('Drag over:', event.target);
-
-    //     event.preventDefault();
-    // }
-
-    // handleDrop(event) {
-    //     // Prevent default action (open as link for some elements)
-    //     event.preventDefault();
+    handleDrop(event) {
+        // Prevent default action (open as link for some elements)
+        event.preventDefault();
         
-    //     // Get the dragged item
-    //     const draggedItemId = event.dataTransfer.getData('text/plain');
-    //     const draggedItem = document.getElementById(draggedItemId);
+        // Get the dragged item
+        const draggedItemId = event.dataTransfer.getData('text/plain');
+        const draggedItem = document.getElementById(draggedItemId);
         
-    //     // Get the drop target
-    //     const dropTarget = event.target.closest('.newItem');
+        // Get the drop target
+        const dropTarget = event.target.closest('.newItem');
         
-    //     console.log('Dragged Item:', draggedItem);
-    //     console.log('Drop Target:', dropTarget);
+        console.log('Dragged Item:', draggedItem);
+        console.log('Drop Target:', dropTarget);
         
-    //     if (dropTarget && draggedItem !== dropTarget) {
-    //         // Insert the dragged item before the drop target
-    //         dropTarget.parentNode.insertBefore(draggedItem, dropTarget);
-    //         console.log('Inserting before:', dropTarget);
-    //     }
+        if (dropTarget && draggedItem !== dropTarget) {
+            // Insert the dragged item before the drop target
+            dropTarget.parentNode.insertBefore(draggedItem, dropTarget);
+            console.log('Inserting before:', dropTarget);
+        }
     
-    //     // Remove the 'dragging' class from the dragged item
-    //     draggedItem.classList.remove('dragging');
-    //     this.saveData();
-    // }
+        // Remove the 'dragging' class from the dragged item
+        draggedItem.classList.remove('dragging');
+        this.saveData();
+    }
 
-    // handleDragEnd(event) {
-    //     // remove the dragging class when drag operation is complete
-    //     event.target.classList.remove('dragging');
-    // }
-
-    darkToggle(event) {
+    darkToggle() {
         document.body.classList.toggle('dark-mode');
         document.querySelector('#movingDiv').classList.toggle('dark-mode');
         document.getElementById('list-container').classList.toggle('dark-mode');
         document.getElementById('title').classList.toggle('dark-mode');
-    
+        
         const newItems = document.querySelectorAll('.newItem');
         const trashButtons = document.querySelectorAll('.trash-button');
     
